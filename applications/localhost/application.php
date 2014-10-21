0f829c9e19710f7946f7f1aaccf2e5da5b8cb7d4
bkalaycioglu <thecodemasterzz@gmail.com>
2014-10-21 22:55:58 +0300

    i temporarily add some code block. i will change these global variable to the getting from config file

--- applications/localhost/application.php
+++ applications/localhost/application.php
@@ -6,3 +6,7 @@
  * @package  architect
  * @author   Baris Kalaycioglu <thecodemasterzz@gmail.com>
  */
+
+# @TODO: Before doing application package, i need to add temporarily added following sections.
+define("BASE_URL", 		'http://project-bk/' );
+define("THEME_NAME", 	'default' );
