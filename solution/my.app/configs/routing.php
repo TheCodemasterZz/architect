b4d9c95c2cd829321ea8d4c7e0d4e7f3d07753d1
thecodemasterzz <thecodemasterzz@gmail.com>
2015-10-25 20:58:26 +0200

    new

--- solution/my.app/configs/routing.php
+++
@@ -1,7 +0,0 @@
-<?php
- 
-//Read the annotations from UsersController if the uri starts with /api/users
-$router->addResource('RefCities', '/api/ref/cities');
-
-//Return all routing settings
-return $router;
