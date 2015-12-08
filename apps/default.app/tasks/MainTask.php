3ce537315631dd9212d3abb69044e36e42f68b61
thecodemasterzz <thecodemasterzz@gmail.com>
2015-12-08 15:46:14 +0200

    i add task classes

---
+++ apps/default.app/tasks/MainTask.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+* architect - a PHP Framework for rapid developing
+*
+* @package  architect
+* @author   Baris Kalaycioglu <thecodemasterzz@gmail.com>
+*/
+
+class MainTask extends \Phalcon\CLI\Task
+{
+    public function mainAction()
+    {
+    	
+    }
+}
